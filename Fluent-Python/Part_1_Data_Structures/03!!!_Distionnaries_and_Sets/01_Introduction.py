83f23af148fe1cbb344e688933c763e22e87083d
Julien Marty <julienmarty0819@gmail.com>
2026-02-08 17:43:50 +0100

    Started chapter 3 of fluent python

---
+++ Fluent-Python/Part_1_Data_Structures/03!!!_Distionnaries_and_Sets/01_Introduction.py
@@ -0,0 +1,7 @@
+""""""
+"""
+We use dictionaries in all our Python programs. If not directly in our code, then indirectly because the dict type is a 
+fundamental part of Python’s implementation. Class and instance attributes, module namespaces, and function keyword 
+arguments are some of the core Python constructs represented by dictionaries in memory. The __builtins__.__dict__ stores
+all built-in types, objects, and functions.
+"""
---
+++ Fluent-Python/Part_1_Data_Structures/03!!!_Distionnaries_and_Sets/02_Modern_Dict_Syntax.py
@@ -0,0 +1,36 @@
+"""
+Dict comprehension (same as list comprehension and generator expression)
+"""
+dial_codes = [
+    (92, 'Pakistan'),
+    (7, 'Russia'),
+    (1, 'United States'),
+]
+
+country_dial = {country: code for code, country in dial_codes}
+print(country_dial)
+
+refined_country_dial = {country.upper(): code
+                        for country, code in sorted(country_dial.items())
+                        if code < 10}
+print(refined_country_dial)
+
+"""
+Unpacking mapping
+
+We can apply ** to more than one argument in a function call. This works when keys are all strings and unique across all 
+arguments (because duplicate keyword arguments are forbidden).
+"""
+
+def dump(**kwargs):
+    print(kwargs)
+
+dump(**{"a": 1,
+        "b": 2},
+     c=3,
+     **{"d": 4,
+        "e": 5},
+    )
+
+# /!\ In this case, duplicate keys are allowed. Later occurrences overwrite previous ones.
+dump(**{'a': 0, **{'x': 1}, 'y': 2, **{'z': 3, 'x': 4}})
